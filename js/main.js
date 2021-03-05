c3e45f07263ee88e9b7e66d91063b2da75a3d564
Javier N. Pérez <73880396+jnfire@users.noreply.github.com>
2021-03-05 18:00:07 +0100

    Se puede navegar por los menus

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-
+// Fichas
 const fichaBlanca = 1;
 const fichaNegra = 2;
 const casillaVacia = 0;
 
+// Tablero de nueva partida
 const tableroInicial = [
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,6 +15,14 @@ const tableroInicial = [
     [0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+/*
+* Menus:
+    * Inicio: 1
+    * Ajustes: 2
+    * Menu: 3
+    * Victoria: 4
+* */
+
 
 new Vue({
     el: '#app',
@@ -23,15 +32,28 @@ new Vue({
         fichaNegra: fichaNegra,
         casillaVacia: casillaVacia,
 
-        desplegado: true
-        menu:
+        desplegado: true,
+        menu: 1,
+        victoria: 0,
+
+        jugador1: true,
+        jugador2: false,
+        jugadorActivo: true
     },
     mounted: function () {
     },
     methods: {
         nuevaPartida: function () {
             this.tableroJuego = tableroInicial;
+            this.desplegado = false;
+            this.menu = 3;
         },
+        turno: function () {
+            this.jugadorActivo = !this.jugadorActivo;
+        },
+        colocarFicha: function () {
+
+        }
     },
     computed: {
 
